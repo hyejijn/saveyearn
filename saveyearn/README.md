3b7650f48eb5a456d72bd4623573a752084fabb4
hyejijn <gpwlss79441@gmail.com>
2025-05-07 10:36:50 +0900

    Add files via upload

---
+++ saveyearn/README.md
@@ -0,0 +1,1 @@
+# saveyearn
---
+++ saveyearn/assets/js/main.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 100,
+    freeMode: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
+      },
+  });
+  
